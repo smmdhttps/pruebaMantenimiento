70fb0907c364592148b2d920464b6b3160d6f263
Sara Muñoz <s@MacBook-Air-de-Sara.local>
2026-09-12 10:35:54 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pruebaMantenimiento',
+  webDir: 'www'
+};
+
+export default config;
